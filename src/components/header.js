bb869083d567153c35c3026ee3c45095740b9c4b
lefelaR <rakgew@gmail.com>
2021-01-23 18:20:03 +0200

    added my own styles for the project

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import '../css/styles.css';
+
+class Header extends React.Component{
+
+    render(){
+        return(
+            <h1>
+                Logo
+            </h1>
+        )
+        }
+}
+
+export default Header;
