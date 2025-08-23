ceae5ef2749e1475073d323564f48d0e80ef01d2
lefelaR <rakgew@gmail.com>
2025-08-23 21:51:51 +0200

    top nav from vite and tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { useState} from 'react'
+import TopNav from '../../components/templates/navs/TopNav';
+
+const Home = () => {
+  return (
+        <TopNav />
+  );
+}
+
+export default Home;
